e2384e1a693b5ce141b67cb26b65f2277eb751e7
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-11-01 19:43:38 +0530

    setup server

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
+import logger from './utils/logger.js';
+import { ENV } from './config/env.js';
+
+const env = process.env.NODE_ENV || ENV.DEVELOPMENT;
+
+let envFile = '.env.dev';
+if (env === ENV.PRODUCTION) {
+    envFile = '.env.prod';
+} else if (env === ENV.STAGING) {
+    envFile = '.env.stage';
+}
+
+dotenv.config({ path: envFile });
+
+const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => logger.info(`Server running in ${env} mode on port ${PORT}`));
