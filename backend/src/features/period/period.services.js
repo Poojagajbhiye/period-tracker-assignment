e956db870df63891a2e58df9f6e270a1acbd1f0c
Pooja Gajbhiye <poojagajbhiye563@gmail.com>
2025-11-01 22:48:50 +0530

    add create period service

---
+++ backend/src/features/period/period.services.js
@@ -0,0 +1,11 @@
+import Period from './period.model';
+
+export const createPeriod = async (data) => {
+  try {
+    const period = new Period(data);
+    await period.save();
+    return period;
+  } catch (error) {
+    throw new Error(`Failed to create period: ${error.message}`);
+  }
+};
